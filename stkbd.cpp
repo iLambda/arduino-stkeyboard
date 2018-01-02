1f3516a3f5bf755340550c519bfe2cf9e652fd82
iLambda <me@ilambda.io>
2018-01-02 11:34:21 +0100

    Added source files.

---
+++ stkbd.cpp
@@ -0,0 +1,218 @@
+/*
+ * stkbd.cpp - a library to interface with an Atari ST keyboard.
+ * Written by Ada Freeman (iLambda2), December 2017.
+ * Release into public domain.
+ */
+
+#include "stkbd.h"
+
+
+// ST to PS/2 keycode map
+extern uint8_t stToPS2[] = {
+  0x0  /* 0 */,
+  0x1  /* 1 */,
+  0x2  /* 2 */,
+  0x3  /* 3 */,
+  0x4  /* 4 */,
+  0x5  /* 5 */,
+  0x6  /* 6 */,
+  0x7  /* 7 */,
+  0x8  /* 8 */,
+  0x9  /* 9 */,
+  0xA  /* A */,
+  0xB  /* B */,
+  0xC  /* C */,
+  0xD  /* D */,
+  0xE  /* E */,
+  0xF  /* F */,
+  0x10  /* 10 */,
+  0x11  /* 11 */,
+  0x12  /* 12 */,
+  0x13  /* 13 */,
+  0x14  /* 14 */,
+  0x15  /* 15 */,
+  0x16  /* 16 */,
+  0x17  /* 17 */,
+  0x18  /* 18 */,
+  0x19  /* 19 */,
+  0x1A  /* 1A */,
+  0x1B  /* 1B */,
+  0x1C  /* 1C */,
+  0x1D  /* 1D */,
+  0x1E  /* 1E */,
+  0x1F  /* 1F */,
+  0x20  /* 20 */,
+  0x21  /* 21 */,
+  0x22  /* 22 */,
+  0x23  /* 23 */,
+  0x24  /* 24 */,
+  0x25  /* 25 */,
+  0x26  /* 26 */,
+  0x27  /* 27 */,
+  0x28  /* 28 */,
+  0x29  /* 29 */,
+  0x2A  /* 2A */,
+  0x2B  /* 2B */,
+  0x2C  /* 2C */,
+  0x2D  /* 2D */,
+  0x2E  /* 2E */,
+  0x2F  /* 2F */,
+  0x30  /* 30 */,
+  0x31  /* 31 */,
+  0x32  /* 32 */,
+  0x33  /* 33 */,
+  0x34  /* 34 */,
+  0x35  /* 35 */,
+  0x36  /* 36 */,
+  0x0  /* 37 */,
+  0x38  /* 38 */,
+  0x39  /* 39 */,
+  0x3A  /* 3A */,
+  0x3B  /* 3B */,
+  0x3C  /* 3C */,
+  0x3D  /* 3D */,
+  0x3E  /* 3E */,
+  0x3F  /* 3F */,
+  0x40  /* 40 */,
+  0x41  /* 41 */,
+  0x42  /* 42 */,
+  0x43  /* 43 */,
+  0x44  /* 44 */,
+  0x0  /* 45 */,
+  0x0  /* 46 */,
+  0x0  /* 47 */,
+  0x0  /* 48 */,
+  0x0  /* 49 */,
+  0x4A  /* 4A */,
+  0x0  /* 4B */,
+  0x0  /* 4C */,
+  0x0  /* 4D */,
+  0x4E  /* 4E */,
+  0x0  /* 4F */,
+  0x0  /* 50 */,
+  0x0  /* 51 */,
+  0x0  /* 52 */,
+  0x0  /* 53 */,
+  0x0  /* 54 */,
+  0x0  /* 55 */,
+  0x0  /* 56 */,
+  0x0  /* 57 */,
+  0x0  /* 58 */,
+  0x0  /* 59 */,
+  0x0  /* 5A */,
+  0x0  /* 5B */,
+  0x0  /* 5C */,
+  0x0  /* 5D */,
+  0x0  /* 5E */,
+  0x0  /* 5F */,
+  0x0  /* 60 */,
+  0x0  /* 61 */,
+  0x0  /* 62 */,
+  0x0  /* 63 */,
+  0x0  /* 64 */,
+  0x37  /* 65 */,
+  0x37  /* 66 */,
+  0x47  /* 67 */,
+  0x48  /* 68 */,
+  0x49  /* 69 */,
+  0x4B  /* 6A */,
+  0x4C  /* 6B */,
+  0x4D  /* 6C */,
+  0x4F  /* 6D */,
+  0x50  /* 6E */,
+  0x51  /* 6F */,
+  0x52  /* 70 */,
+  0x53  /* 71 */,
+  0x1C  /* 72 */
+};
+
+STKeyboard::STKeyboard(HardwareSerial* serial, int16_t pinLight, int16_t pinReset) {
+    // save pins
+    this->m_serial = serial;
+    this->m_pinLight = pinLight;
+    this->m_pinReset = pinReset;
+}
+
+
+void STKeyboard::initialize() {
+    // set pins as OUTPUT
+    pinMode(this->m_pinLight, OUTPUT);
+    pinMode(this->m_pinReset, OUTPUT);
+    digitalWrite(this->m_pinReset, HIGH);
+    // turn off light
+    this->setFloppyLight(false);
+    // initialize the serial communication
+    this->m_serial->begin(STKBD_baudrate);
+    // reset stkbd
+    delay(200);
+    this->reset();
+    delay(200);
+}
+
+int16_t STKeyboard::available() {
+    return this->m_serial->available();
+}
+
+uint8_t STKeyboard::read() {
+    return this->m_serial->read();
+}
+
+void STKeyboard::reset() {
+    // bang RST pin
+    digitalWrite(this->m_pinReset, HIGH);
+    delay(20);
+    digitalWrite(this->m_pinReset, LOW);
+    delay(20);
+    digitalWrite(this->m_pinReset, HIGH);
+    // send command
+    this->m_serial->write(0x80);
+    this->m_serial->write(0x01);
+    // remove all garbage data in serial
+    delay(500);
+    while(this->available()) this->read();
+}
+
+bool STKeyboard::getFloppyLight() { return m_lightState; }
+void STKeyboard::setFloppyLight(bool value) {
+    // save state
+    m_lightState = value;
+    // write to pin
+    digitalWrite(this->m_pinLight, value ? LOW : HIGH);
+}
+
+
+bool STKeyboard::isMake(uint8_t code) { return !STKeyboard::isBreak(code); } 
+bool STKeyboard::isBreak(uint8_t code) { return !!(code & 0x80); }
+uint8_t STKeyboard::getKey(uint8_t code) { return code & 0x7F; }
+uint32_t STKeyboard::toPS2Keycode(uint8_t code) {
+  uint32_t res;
+    uint8_t key = STKeyboard::getKey(code);
+  // get actual key
+  res |= stToPS2[key];
+  // check if up, down, left, right arrow
+  if (key == STKBD_KEY_UP ||
+      key == STKBD_KEY_DOWN  ||
+      key == STKBD_KEY_LEFT || 
+      key == STKBD_KEY_RIGHT) {
+    // check key
+    uint8_t arrowkey = 0x75;
+    switch(key) {
+      case STKBD_KEY_UP: arrowkey = 0x75; break;
+      case STKBD_KEY_DOWN: arrowkey = 0x72; break;
+      case STKBD_KEY_LEFT: arrowkey = 0x6B; break;
+      case STKBD_KEY_RIGHT: arrowkey = 0x74; break;
+    }
+    // special code 
+    res |= (((uint32_t)0xE0) << (STKeyboard::isBreak(code) ? 16 : 8));
+    res |= arrowkey;
+  }
+  // add break code
+  if (STKeyboard::isBreak(code)) {
+    res |= (((uint32_t)0xF0) << 8);    
+  }
+  
+  
+  // return result
+  return res;
+}
+
---
+++ stkbd.h
@@ -0,0 +1,148 @@
+/*
+ * stkbd.h - a library to interface with an Atari ST keyboard.
+ * Written by Ada Freeman (iLambda2), December 2017.
+ * Release into public domain.
+ */
+
+#ifndef _STKBD_H
+#define _STKBD_H
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <HardwareSerial.h>
+
+// ATARI ST Rx/Tx baud rate
+#define STKBD_baudrate  7812
+
+// keycodes definitions
+#define STKBD_KEY_ESC            0x01
+#define STKBD_KEY_1              0x02
+#define STKBD_KEY_2              0x03
+#define STKBD_KEY_3              0x04
+#define STKBD_KEY_4              0x05
+#define STKBD_KEY_5              0x06
+#define STKBD_KEY_6              0x07
+#define STKBD_KEY_7              0x08
+#define STKBD_KEY_8              0x09
+#define STKBD_KEY_9              0x0A
+#define STKBD_KEY_0              0x0B
+#define STKBD_KEY_HYPHEN         0x0C
+#define STKBD_KEY_EQUAL          0x0D
+#define STKBD_KEY_BS             0x0E
+#define STKBD_KEY_TAB            0x0F
+#define STKBD_KEY_Q              0x10
+#define STKBD_KEY_W              0x11
+#define STKBD_KEY_E              0x12
+#define STKBD_KEY_R              0x13
+#define STKBD_KEY_T              0x14
+#define STKBD_KEY_Y              0x15
+#define STKBD_KEY_U              0x16
+#define STKBD_KEY_I              0x17
+#define STKBD_KEY_O              0x18
+#define STKBD_KEY_P              0x19
+#define STKBD_KEY_LEFTBRACE      0x1A
+#define STKBD_KEY_RIGHTBRACE     0x1B
+#define STKBD_KEY_RET            0x1C
+#define STKBD_KEY_CTRL           0x1D
+#define STKBD_KEY_A              0x1E
+#define STKBD_KEY_S              0x1F
+#define STKBD_KEY_D              0x20
+#define STKBD_KEY_F              0x21
+#define STKBD_KEY_G              0x22
+#define STKBD_KEY_H              0x23
+#define STKBD_KEY_J              0x24
+#define STKBD_KEY_K              0x25
+#define STKBD_KEY_L              0x26
+#define STKBD_KEY_SEMICOLON      0x27
+#define STKBD_KEY_APOSTROPHE     0x28
+#define STKBD_KEY_BACKTICK       0x29
+#define STKBD_KEY_LEFT_SHIFT     0x2A
+#define STKBD_KEY_ANTISLASH      0x2B
+#define STKBD_KEY_Z              0x2C
+#define STKBD_KEY_X              0x2D
+#define STKBD_KEY_C              0x2E
+#define STKBD_KEY_V              0x2F
+#define STKBD_KEY_B              0x30
+#define STKBD_KEY_N              0x31
+#define STKBD_KEY_M              0x32
+#define STKBD_KEY_COMMA          0x33
+#define STKBD_KEY_DOT            0x34
+#define STKBD_KEY_SLASH          0x35
+#define STKBD_KEY_RIGHT_SHIFT    0x36
+#define STKBD_KEY_ALT            0x38
+#define STKBD_KEY_SPACE_BAR      0x39
+#define STKBD_KEY_CAPS_LOCK      0x3A
+#define STKBD_KEY_F_1            0x3B
+#define STKBD_KEY_F_2            0x3C
+#define STKBD_KEY_F_3            0x3D
+#define STKBD_KEY_F_4            0x3E
+#define STKBD_KEY_F_5            0x3F
+#define STKBD_KEY_F_6            0x40
+#define STKBD_KEY_F_7            0x41
+#define STKBD_KEY_F_8            0x42
+#define STKBD_KEY_F_9            0x43
+#define STKBD_KEY_F_10           0x44
+#define STKBD_KEY_HOME           0x47
+#define STKBD_KEY_UP                     0x48
+#define STKBD_KEY_KEYPAD                 0x4A
+#define STKBD_KEY_LEFT                   0x4B
+#define STKBD_KEY_RIGHT                  0x4D
+#define STKBD_KEY_KEYPAD                 0x4E
+#define STKBD_KEY_DOWN                   0x50
+#define STKBD_KEY_INSERT                 0x52
+#define STKBD_KEY_DEL                    0x53
+#define STKBD_KEY_ISO_KEY                0x60
+#define STKBD_KEY_UNDO                   0x61
+#define STKBD_KEY_HELP                   0x62
+#define STKBD_KEY_KEYPAD_LEFTBRACKET     0x63
+#define STKBD_KEY_KEYPAD_SLASH           0x64
+#define STKBD_KEY_KEYPAD_ASTERISK        0x65
+#define STKBD_KEY_KEYPAD_RIGHTBRACKET    0x66
+#define STKBD_KEY_KEYPAD_7               0x67
+#define STKBD_KEY_KEYPAD_8               0x68
+#define STKBD_KEY_KEYPAD_9               0x69
+#define STKBD_KEY_KEYPAD_4               0x6A
+#define STKBD_KEY_KEYPAD_5               0x6B
+#define STKBD_KEY_KEYPAD_6               0x6C
+#define STKBD_KEY_KEYPAD_1               0x6D
+#define STKBD_KEY_KEYPAD_2               0x6E
+#define STKBD_KEY_KEYPAD_3               0x6F
+#define STKBD_KEY_KEYPAD_0               0x70
+#define STKBD_KEY_KEYPAD_DOT             0x71
+#define STKBD_KEY_KEYPAD_ENTER           0x72
+
+class STKeyboard {
+
+    private:
+        HardwareSerial* m_serial;
+        int16_t m_pinLight;
+        int16_t m_pinReset;
+        bool m_lightState;
+
+    public:
+        STKeyboard(HardwareSerial* serial, int16_t pinLight, int16_t pinReset);
+
+    public:
+        void initialize();
+        int16_t available();
+        uint8_t read();
+        void reset();
+
+    public:
+        bool getFloppyLight();
+        void setFloppyLight(bool value);
+        static bool isMake(uint8_t code);
+        static bool isBreak(uint8_t code);
+        static uint8_t getKey(uint8_t code);
+        static uint32_t toPS2Keycode(uint8_t code);
+
+    private:
+
+
+
+};
+
+#endif /* _STKBD_H */
